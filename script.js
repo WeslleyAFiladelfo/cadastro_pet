2c39a9652b1917c452516a692091c88aacc46e42
WeslleyAFiladelfo <67105552+WeslleyAFiladelfo@users.noreply.github.com>
2024-04-26 06:29:57 -0300

    Adicionando pasta Cadastro_pet

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('product-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
-    // You can add code here to handle form submission, e.g., sending data to the server
-    alert('Form submitted!'); // For demonstration
-});
